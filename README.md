8ceb7bc630f4bead793e359fa717b7081d7ff167
Alex Nichol <unixpickle@gmail.com>
2016-05-24 13:08:47 -0400

    packaged MNIST binary data

---
+++ README.md
@@ -0,0 +1,3 @@
+# mnist
+
+This is a Go package for testing classifiers on the [MNIST database of handwritten digits](http://yann.lecun.com/exdb/mnist/).
---
+++ bindata.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+#
+# This script downloads the MINST database
+# and packages it in a .go file.
+#
+# You shouldn't need to run this script,
+# since the .go file is in the repository.
+#
+
+rm -rf data
+mkdir data
+curl http://yann.lecun.com/exdb/mnist/train-images-idx3-ubyte.gz >data/train-images-idx3-ubyte.gz
+curl http://yann.lecun.com/exdb/mnist/train-labels-idx1-ubyte.gz >data/train-labels-idx1-ubyte.gz
+curl http://yann.lecun.com/exdb/mnist/t10k-images-idx3-ubyte.gz >data/t10k-images-idx3-ubyte.gz
+curl http://yann.lecun.com/exdb/mnist/t10k-labels-idx1-ubyte.gz >data/t10k-labels-idx1-ubyte.gz
+go-bindata -o bindata.go -nocompress data/
+rm -rf data
