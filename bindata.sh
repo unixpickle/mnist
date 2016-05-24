021e542d8ccae85294978aeda22256e20f923de9
Alex Nichol <unixpickle@gmail.com>
2016-05-24 13:12:33 -0400

    use correct package name in bindata

--- bindata.sh
+++ bindata.sh
@@ -13,5 +13,5 @@ curl http://yann.lecun.com/exdb/mnist/train-images-idx3-ubyte.gz >data/train-ima
 curl http://yann.lecun.com/exdb/mnist/train-labels-idx1-ubyte.gz >data/train-labels-idx1-ubyte.gz
 curl http://yann.lecun.com/exdb/mnist/t10k-images-idx3-ubyte.gz >data/t10k-images-idx3-ubyte.gz
 curl http://yann.lecun.com/exdb/mnist/t10k-labels-idx1-ubyte.gz >data/t10k-labels-idx1-ubyte.gz
-go-bindata -o bindata.go -nocompress data/
+go-bindata -pkg mnist -o bindata.go -nocompress data/
 rm -rf data
